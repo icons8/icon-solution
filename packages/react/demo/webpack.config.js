855f11fe6129ade1cfe432c8d24b6dc391da5105
Viacheslav Bereza <betula.icons8@gmail.com>
2017-03-16 13:42:09 +0300

    Remove styles from webpack

--- packages/react/demo/webpack.config.js
+++ packages/react/demo/webpack.config.js
@@ -5,45 +5,14 @@ const
 
 const config = {
 
-  entry: path.resolve(__dirname, './src/Icon.js'),
+  entry: path.resolve(__dirname, './src/index.js'),
 
   module: {
     rules: [{
       test: /\.jsx?$/,
       use: 'babel-loader',
       exclude: /node_modules/
-    }, {
-      test: /\.sass$/,
-      use: [{
-        loader: 'style-loader'
-      }, {
-        loader: 'css-loader',
-        options: {
-          sourceMap: true,
-          modules: true,
-          localIdentName: '[name]__[local]_[hash:3]'
-        }
-      }, {
-        loader: 'postcss-loader',
-        options: {
-          sourceMap: true,
-          plugins: () => [
-            require('autoprefixer')({ grid: false })
-          ]
-        }
-      }, {
-        loader: 'sass-loader',
-        options: {
-          sourceMap: true
-        }
-      }]
     }]
-  },
-
-  resolve: {
-    modules: [
-      path.resolve(__dirname, './components')
-    ]
   },
 
   plugins: [
